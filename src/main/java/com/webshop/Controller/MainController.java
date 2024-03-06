e458d6b57bfd232554e849cbebb035f02dc758e3
KarolWojnar <karolwojnar47@gmail.com>
2024-03-06 23:58:18 +0100

    add link to product page and init product page

--- src/main/java/com/webshop/Controller/MainController.java
+++ src/main/java/com/webshop/Controller/MainController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,5 +16,11 @@ public class MainController {
     public String returnHome(Model model) {
         productService.getAllProducts(model);
         return "home";
+    }
+
+    @GetMapping("/product/{id}")
+    public String returnProductPage(@PathVariable int id, Model model) {
+        productService.returnProduct(id, model);
+        return "product";
     }
 }
--- src/main/java/com/webshop/Service/ProductService.java
+++ src/main/java/com/webshop/Service/ProductService.java
@@ -3,8 +3,11 @@ package com.webshop.Service;
 import com.webshop.Model.Product;
 import com.webshop.Repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +16,13 @@ public class ProductService {
     public void getAllProducts(Model model) {
         Iterable<Product> products = productRepository.getAllProducts();
         model.addAttribute("products", products);
+    }
+
+    public void returnProduct(int id, Model model) {
+        Optional<Product> optional = productRepository.findById((long) id);
+        if (optional.isPresent()) {
+            Product product = optional.get();
+            model.addAttribute("product", product);
+        } else model.addAttribute("notFound", "Product Not Found");
     }
 }
