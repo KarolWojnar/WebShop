bc56b5521ce438afb81acc6ea49ad8145401a600
KarolWojnar <karolwojnar47@gmail.com>
2024-03-26 23:54:34 +0100

    remove not usabel methods

--- src/main/java/com/webshop/Repository/UserRepository.java
+++ src/main/java/com/webshop/Repository/UserRepository.java
@@ -13,8 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     @Query("Select u from User u left join fetch u.roles")
     List<User> findUsers();
-
-    Optional<User> findUserByEmail(String email);
     Optional<User> findByUsernameOrEmail(String username, String email);
     Optional<User> getUserByUserId(int id);
 }
